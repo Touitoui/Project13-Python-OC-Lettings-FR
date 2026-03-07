6139260ef279b46d86e33eba7e98c1fd170d421c
Mickael <mickael.gresse@gmail.com>
2026-03-07 10:38:03 +0100

    1_f Moved templates to corresponding apps, updated views correspondingly

--- lettings/views.py
+++ lettings/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render
 from lettings.models import Letting
 
-def lettings_index(request):
+def index(request):
     lettings_list = Letting.objects.all()
     context = {'lettings_list': lettings_list}
-    return render(request, 'lettings_index.html', context)
+    return render(request, './lettings/index.html', context)
 
 def letting(request, letting_id):
     letting = Letting.objects.get(id=letting_id)
@@ -12,4 +12,4 @@ def letting(request, letting_id):
         'title': letting.title,
         'address': letting.address,
     }
-    return render(request, 'letting.html', context)
+    return render(request, './lettings/letting.html', context)
--- oc_lettings_site/urls.py
+++ oc_lettings_site/urls.py
@@ -7,9 +7,9 @@ from profiles import views as profiles_views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('lettings/', lettings_views.lettings_index, name='lettings_index'),
+    path('lettings/', lettings_views.index, name='lettings_index'),
     path('lettings/<int:letting_id>/', lettings_views.letting, name='letting'),
-    path('profiles/', profiles_views.profiles_index, name='profiles_index'),
+    path('profiles/', profiles_views.index, name='profiles_index'),
     path('profiles/<str:username>/', profiles_views.profile, name='profile'),
     path('admin/', admin.site.urls),
 ]
