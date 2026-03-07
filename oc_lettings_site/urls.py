966167a48f5d00b703b61cdc4a98e23df4b823f9
Mickael <mickael.gresse@gmail.com>
2026-03-07 12:17:28 +0100

    2_c Simple 404 and 500 pages created, uses extends "base.html"

--- oc_lettings_site/urls.py
+++ oc_lettings_site/urls.py
@@ -1,9 +1,19 @@
 from django.contrib import admin
 from django.urls import path
+import django.views.defaults
 
 from . import views
 from lettings import views as lettings_views
 from profiles import views as profiles_views
+
+
+def custom_page_not_found(request):
+    return django.views.defaults.page_not_found(request, None)
+
+
+def custom_server_error(request):
+    return django.views.defaults.server_error(request)
+
 
 urlpatterns = [
     path('', views.index, name='index'),
@@ -12,4 +22,7 @@ urlpatterns = [
     path('profiles/', profiles_views.index, name='profiles_index'),
     path('profiles/<str:username>/', profiles_views.profile, name='profile'),
     path('admin/', admin.site.urls),
+
+    path("404/", custom_page_not_found),
+    path("500/", custom_server_error),
 ]
