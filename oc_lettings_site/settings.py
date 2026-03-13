39506410a122eb048278d1b6d68c1fdf7309d117
Mickael Gresse <vysmop@gmail.com>
2026-03-13 05:32:48 +0100

    fix: create STATIC_ROOT folder to fix warning in pytest

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -118,6 +118,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+os.makedirs(STATIC_ROOT, exist_ok=True)
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / "static", ]
